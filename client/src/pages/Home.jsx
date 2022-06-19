87fd0bb0054665b97e24d0a4d5b6305c0ef407bc
Josh Diamond <joshdiamondthedev@gmail.com>
2022-06-18 21:39:09 -0700

    first-commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Home.css'
+
+export const Home = () => {
+
+    return (
+        <div className='home'>
+            <h2 className='home-title'>Home Page</h2>
+        </div>
+    )
+}
---
+++ client/src/pages/Manage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Manage.css'
+
+export const Manage = () => {
+
+    return (
+        <div className='manage'>
+            <h2 className='manage-title'>Manage Page</h2>
+        </div>
+    )
+}
