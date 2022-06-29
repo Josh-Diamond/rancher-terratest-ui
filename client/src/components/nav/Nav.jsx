a48ce5527e3712dda801f2c6cae820f18de45db7
Josh Diamond <joshdiamondthedev@gmail.com>
2022-06-28 21:43:31 -0700

    hackweek_day2_afterhours

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import '../../styles/Nav.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilePen, faFolderOpen, faGears, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
-
+import { faBars, faFilePen, faFolderOpen, faGears, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import rancher from '../../static/rbf.png'
 
 export const Nav = ({setAuth}) => {
 
@@ -16,26 +16,27 @@ export const Nav = ({setAuth}) => {
 
     return (
         <div className='nav'>
-            <h1 className='nav-title'> Rancher • Terratest</h1>
-            <div className='links'>
-            <div className='config-nav'>
-                    <FontAwesomeIcon icon={faGears} />
-                    <Link to="/config" className='config-link'>Config.go</Link>
+                <Link to="/home">
+                <img src={rancher} className='rancher' />
+                </Link>
+                <div className='links'>
+                <div className='config-nav'>
+                        <FontAwesomeIcon icon={faGears} />
+                        <Link to="/config" className='config-link'>CONFIG</Link>
+                    </div>
+                    <div className='create-nav'>
+                        <FontAwesomeIcon icon={faFilePen} />
+                        <Link to="/create" className='create-link'>CREATE</Link>
+                    </div>
+                    <div className='manage-nav'>
+                    <FontAwesomeIcon icon={faFolderOpen} />
+                        <Link to="/manage" className='manage-link'>MANAGE</Link>
+                    </div>
+                    <div onClick={() => Logout()} className='logout-nav'>
+                    <FontAwesomeIcon icon={faRightFromBracket} />
+                        <Link to="/" className='logout-link'>LOGOUT</Link>
+                    </div>
                 </div>
-                <div className='create-nav'>
-                    <FontAwesomeIcon icon={faFilePen} />
-                    <Link to="/create" className='create-link'>CREATE</Link>
-                </div>
-                <div className='manage-nav'>
-                <FontAwesomeIcon icon={faFolderOpen} />
-                    <Link to="/manage" className='manage-link'>MANAGE</Link>
-                </div>
-                <div onClick={() => Logout()} className='logout-nav'>
-                <FontAwesomeIcon icon={faRightFromBracket} />
-                    <Link to="/" className='logout-link'>LOGOUT</Link>
-                </div>
-            </div>
-
         </div>
     )
 }
